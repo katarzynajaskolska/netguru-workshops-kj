d8e9a179be552381c597fc190ecb6433ec775b5b
Katarzyna Jaskólska <katarzynaj846@gmail.com>
2015-04-09 16:46:49 +0200

    [#4] Make product_spec.rb model spec pass
    - add validations for title, description and price to be present
    - add validation for price format
    - add average_rating method

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,12 @@ class Product < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
   has_many :reviews
+
+  validates_presence_of :title, :description, :price
+  validates :price, format: {with: /\A\d+(?:\.\d{0,2})?\z/}
+
+  def average_rating
+    self.reviews.map{|review| review.rating}.inject(:+).fdiv(self.reviews.length)
+  end
+
 end
