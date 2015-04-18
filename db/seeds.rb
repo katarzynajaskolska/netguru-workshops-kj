5b27eb3d96e67fbd57ebca558b2a25e09eae55e4
Katarzyna Jaskólska <katarzynaj846@gmail.com>
2015-04-18 19:00:53 +0200

    [#16] Seed the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(
+  admin: true,
+  firstname: Faker::Name.first_name,
+  lastname: Faker::Name.last_name,
+  email: 'admin@example.com',
+  password: 'password')
+
+5.times do |n|
+  User.create(
+    firstname: Faker::Name.first_name,
+    lastname: Faker::Name.last_name,
+    email: "user#{n+1}@example.com",
+    password: 'password')
+end
+
+Category.create(name: 'phones')
+
+10.times do |n|
+  Product.create(
+    title: Faker::Product.model,
+    description: Faker::Lorem.sentence,
+    price: rand(1000),
+    category_id: 1,
+    user_id: rand(1..6))
+end
+
+30.times do |n|
+  Review.create(
+    content: Faker::Lorem.sentence,
+    rating: rand(1..5),
+    product_id: rand(1..10),
+    user_id: rand(1..6))
+end
